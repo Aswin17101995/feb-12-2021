358a72033918d0433dc032801614144de8d0888a
Aswin17101995 <78901936+Aswin17101995@users.noreply.github.com>
2021-02-17 11:23:06 +0530

    Add files via upload

--- func.js
+++ func.js
@@ -176,3 +176,178 @@ var result=function(a,b)
     return a;
 };
 console.log(result([1,2,3,4,5,6,7,8,9],2));
+//============================================================================
+
+//iffe
+//Print odd numbers in an array 
+var result=function(a)
+{
+    var ans=[];
+    for(var i in a)
+    {
+        if(a[i]%2===0)
+        {
+            ans.push(a[i]);
+        }
+    }
+    return ans;
+}([1,2,3,4,5]);
+console.log(result);
+
+
+//Convert all the strings to title caps in a string array
+var result=function(a)
+{
+    var ans=[];
+    for(var i in a)
+    {
+        ans[i]=a[i].toUpperCase();
+    }
+    return ans;
+}(["aswin","srini","mass","weight"]);
+console.log(result);
+
+// Sum of all numbers in an array 
+var result=function(a)
+{
+    var ans=0
+    for(var i in a)
+    {
+        ans+=a[i];
+    }
+    return ans;
+}([1,2,3,4,5]);
+console.log(result);
+
+//Return all the prime numbers in an array
+var result=function(a)
+{
+    var ans=[];
+    
+    for(var i in a)
+    {
+        var count=0;
+        for(var j=1;j<=a[i];j++)
+        {
+            if(a[i]%j===0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            ans.push(a[i]);
+        }
+    }
+    return ans;
+}([1,2,3,4,5]);
+console.log(result);
+
+// Return all the palindromes in an array
+var result=function(a)
+{
+    var ans=[];
+    for(var i in a)
+    {
+        var str=a[i];
+        var maxlen=a[i].length;
+        var len=Math.floor(a[i].length/2);
+        var check=0;
+        for(var j=0;j<len;j++)
+        {
+            if(str[j]===str[maxlen-1-j])
+            {
+                continue;
+            }
+            else
+            {
+                check=-1;
+                break;
+            }
+        }
+        if(check!=-1)
+        {
+            ans.push(str);
+        }
+    }
+    return ans;
+}(["1221","0000","anna","rotator"]);
+console.log(result);
+
+
+
+//Remove duplicates from an array 
+var result=function(a)
+{
+    
+    a.sort();
+    console.log(a);
+    var x=[];
+    for(var i=0;i<a.length;)
+    {
+        var temp;
+        var count=0;
+        for(var j=i+1;j<a.length;j++)
+        {
+            console.log("key",a[i]," ","compared to",a[j]);
+            if(j<a.length)
+            {
+            if(a[i]==a[j])
+            {
+                count++;
+                temp=j;
+                
+            }
+            else
+            {
+                break;
+            }
+            }
+            
+        }
+        if(count===0)
+        {
+            console.log("key added",a[i]);
+            x.push(a[i]);
+            i++;
+        }
+        else
+        {
+            console.log(a[i],"repeated");
+            i=temp+1;
+            console.log(i,"index");
+        }
+    }
+    return x;
+}([1,3,3,3,3,3,5,5,5,5,7,7,7,9]);
+console.log(result);
+
+
+//Rotate an array by k times and return the rotated array
+var result=function(a,b)
+{
+    console.log(a);
+    var temp=0;
+    for(var i=0;i<b;i++)
+    {
+        temp=a[0];
+        a[0]=a[a.length-1];
+        
+        for(var j=a.length-1;j>0;j--)
+        {
+            if(j-1===0)
+            {
+                a[1]=temp;
+            }
+            else
+            {
+             a[j]=a[j-1];
+            }
+           
+            
+        }
+        console.log(a);
+    }
+    return a;
+}([1,2,3,4,5,6,7,8,9],2);
+console.log(result);
